a532f4860a54d857bf3230aaec226fdb833a0dd0
Kunal Chahal <chahalkunal2209@gmail.com>
2023-06-15 01:04:39 +0530

    Update insert_image.py

--- insert_image.py
+++ insert_image.py
@@ -8,12 +8,10 @@ from keys import sendgridKey, mongoKey, mailID
 from datetime import datetime
 
 
-# Connect to MongoDB server
 client = pymongo.MongoClient(mongoKey)
 db = client['test']
 collection = db['userdata']
 
-# Take user inputs
 username = input("Enter your username: ")
 email = input("Enter your email: ")
 phone = input("Enter your phone number: ")
@@ -25,11 +23,9 @@ exit_time = "NA"
 
 
 
-# Check if name already exists in database
 if collection.find_one({"username": username}):
     print("Error: username already exists in database.")
 else:
-    # Open camera window
     cam = cv2.VideoCapture(0)
     cv2.namedWindow("Camera", cv2.WINDOW_NORMAL)
     cv2.resizeWindow("Camera", 960, 720)
@@ -42,30 +38,24 @@ else:
             cv2.imshow("Camera", frame)
             cv2.waitKey(1000)
             timer -= 1
-    # Capture image and close window
     ret, frame = cam.read()
     cam.release()
     cv2.destroyAllWindows()
 
-    # Save image to MongoDB
     img_bytes = cv2.imencode('.jpg', frame)[1].tobytes()
     data = {"username": username, "email id": email, "phone number": phone, "image": img_bytes,
             "balance": balance, "entry location": entry_location, "entry time": entry_time,
             "exit location": exit_location, "exit time": exit_time, }
     collection.insert_one(data)
 
-    # Load saved image from MongoDB
     img_array = np.asarray(bytearray(img_bytes), dtype=np.uint8)
     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
 
-    # Detect faces in the image
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     face_locations = face_recognition.face_locations(img_rgb)
     if len(face_locations) > 0:
-        # Draw a box around the face(s)
         for (top, right, bottom, left) in face_locations:
             cv2.rectangle(img, (left, top), (right, bottom), (0, 255, 0), 2)
-        # Display the image with faces detected
         cv2.imshow("Face Detection", img)
         print("success!!  face detected in image.")
 
